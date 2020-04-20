7a39d165c03baadb4c3ae982ed1f9188c49e3605
DandeVasanthi <VASANTHI.DANDE@BIWORLDWIDE.COM>
2020-04-20 14:49:53 -0500

    grid-component using vue-nuxt

---
+++ components/DragableComponent.vue
@@ -0,0 +1,49 @@
+<template>
+ <div>
+   <div class="button-wrapper">
+     <button class="button-class" @click ="addComponent">Add</button>
+   </div>
+  
+  <div class="text-wrapper" v-for="index in count" :key="index">
+    <button class="button-delete" @click="deleteComponent">X</button>
+    <textarea    draggable="true"  class="resizable" placeholder="Enter text here..."></textarea>
+  </div>
+ </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'dragable-component',
+  components: {
+  },
+  data() {
+    return  {
+       count : 1  
+    }
+  },
+  methods: {
+    addComponent(): void {
+      this.count += 1;
+    },
+    deleteComponent(): void {
+      this.count -= 1;
+    }
+  }
+  
+});
+</script>
+<style>
+[draggable=true] {
+  cursor: move;
+}
+
+.resizable {
+  resize: both;
+  border: 1px solid black;
+}
+.button-delete{
+  position: absolute;
+  margin: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+     <dragable-component />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import DragableComponent from '@/components/DragableComponent.vue';
+export default Vue.extend({
+  components: {
+    DragableComponent
+  }
+})
+</script>       
+<style>
+.container {
+  position: relative;
+  display: grid;
+  height: 100%;
+  grid-gap: 1.5rem;
+  grid-template-columns: repeat(12, 1fr);
+  min-height: 100vh;
+  max-height: 100%;
+  overflow-y: auto;
+  grid-auto-rows: 50px 50px 68px auto;
+}
+</style>
